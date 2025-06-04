5b65420b5a15e9cb90e3e915d3f5edbdbd994c8a
GregorySetnikar <gregory.setnikar@protonmail.com>
2025-06-04 13:19:49 +0200

    eu

--- assets/js/include-nav.js
+++ assets/js/include-nav.js
@@ -5,6 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(res => res.text())
             .then(html => {
                 container.innerHTML = html;
+
+                // ➕ Réinitialiser Dropotron après insertion
+                if (window.jQuery) {
+                    $('#nav > ul').dropotron({
+                        alignment: 'right',
+                        mode: 'fade',
+                        noOpenerFade: true
+                    });
+                } else {
+                    console.warn("jQuery non chargé !");
+                }
+
             })
             .catch(err => {
                 console.error("Erreur de chargement du menu :", err);
